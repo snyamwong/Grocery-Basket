61637ce3af9c987fb0b9d8365f331da5b77b227c
Tin Wong <wongt1@wit.edu>
2018-06-17 23:06:42 -0400

    ScrollView, Testing still needed
    Figured out ScrollView (to allow continous scrolling), still needs some testing to ensure it works. Changed button and textview layout, need to get rid of hardcoded text for something more useful (not really needed since they'll be removed anyways). Also, should be adding textview from backend side - same thing with food name and food image.

--- app/src/main/java/edu/wit/mobileapp/mealprepplanner/AddMeal.java
+++ app/src/main/java/edu/wit/mobileapp/mealprepplanner/AddMeal.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 
 /**
@@ -58,6 +59,7 @@ public class AddMeal extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
