add3c1b6593033e0471de519f50b5447edf25b03
Jason Fagerberg <fagerbergj1@wit.edu>
2018-06-21 11:43:34 -0400

    progress but there is a veiw bug for bakery list and after

--- app/src/main/java/edu/wit/mobileapp/mealprepplanner/ShoppingListAdapter.java
+++ app/src/main/java/edu/wit/mobileapp/mealprepplanner/ShoppingListAdapter.java
@@ -1,40 +1,82 @@
 package edu.wit.mobileapp.mealprepplanner;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ShoppingListAdapter extends BaseAdapter {
 
     private Context mContext;
-    private ArrayList<Ingredient> mIngredientList;
+    private ArrayList<Object> mShoppingList;
+    private static final int INGREDIENT = 0;
+    private static final int HEADER =1;
+    private LayoutInflater inflater;
 
-    public ShoppingListAdapter(Context mContext, ArrayList<Ingredient> mIngredientList) {
+    public ShoppingListAdapter(Context mContext, ArrayList<Object> mIngredientList) {
         this.mContext = mContext;
-        this.mIngredientList = mIngredientList;
+        this.mShoppingList = mIngredientList;
+        inflater = (LayoutInflater) mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
+    }
 
+    @Override
+    public int getItemViewType(int position){
+        if(mShoppingList.get(position) instanceof Ingredient){
+            return INGREDIENT;
+        }
+        else
+        {
+            return HEADER;
+        }
+    }
+
+    @Override
+    public int getViewTypeCount(){
+        return 2;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return mShoppingList.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return mShoppingList.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        if(view == null){
+            switch (getItemViewType(i)){
+                case INGREDIENT:
+                    view = inflater.inflate(R.layout.shopping_list, null);
+                    break;
+                case HEADER:
+                    view = inflater.inflate(R.layout.shopping_list_section_headers, null);
+                    break;
+            }
+        }
+        switch (getItemViewType(i)){
+            case INGREDIENT:
+                CheckBox ingredient_chk_box = view.findViewById(R.id.ingredient_chk_box);
+                ingredient_chk_box.setText(((Ingredient) mShoppingList.get(i)).getName());
+                break;
+            case HEADER:
+                TextView ingredient_type_header = view.findViewById(R.id.ingredient_type_header);
+                ingredient_type_header.setText(mShoppingList.get(i).toString());
+                break;
+        }
+        return view;
     }
 }
--- app/src/main/java/edu/wit/mobileapp/mealprepplanner/ShoppingListFragment.java
+++ app/src/main/java/edu/wit/mobileapp/mealprepplanner/ShoppingListFragment.java
@@ -4,6 +4,7 @@ package edu.wit.mobileapp.mealprepplanner;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +14,15 @@ import java.util.ArrayList;
 
 public class ShoppingListFragment extends Fragment {
 
-    private ListView shoppingListView;
-    private ArrayList<Ingredient> mShoppingList;
+    private ListView mShoppingListView;
+    private ArrayList<Object> mShoppingList;
+    private ArrayList<Ingredient> mProduceList;
+    private ArrayList<Ingredient> mBakeryList;
+    private ArrayList<Ingredient> mDeliList;
+    private ArrayList<Ingredient> mMeatList;
+    private ArrayList<Ingredient> mSeafoodList;
+    private ArrayList<Ingredient> mGroceryList;
+    private ArrayList<Ingredient> mDairyList;
     private ShoppingListAdapter adapter;
 
     public ShoppingListFragment() {
@@ -30,19 +38,55 @@ public class ShoppingListFragment extends Fragment {
         //if list isn't initialized
         if(mShoppingList == null) {
             mShoppingList = new ArrayList<>();
-            adapter = new ShoppingListAdapter(getActivity().getApplicationContext(), mShoppingList); //object to update fragment
+            mProduceList = new ArrayList<>();
+            mBakeryList = new ArrayList<>();
+            mDeliList = new ArrayList<>();
+            mMeatList = new ArrayList<>();
+            mSeafoodList = new ArrayList<>();
+            mGroceryList = new ArrayList<>();
+            mDairyList = new ArrayList<>();
 
             for (int i = 0; i <= 11; i++) {
-                mShoppingList.add(new Ingredient("Ingredient " + Integer.toString(i), Integer.toString(i) + "oz"));
+                mProduceList.add(new Ingredient("Ingredient " + Integer.toString(i), Integer.toString(i) + "oz"));
+                mBakeryList.add(new Ingredient("Ingredient " + Integer.toString(i), Integer.toString(i) + "oz"));
+                mDeliList.add(new Ingredient("Ingredient " + Integer.toString(i), Integer.toString(i) + "oz"));
+                mMeatList.add(new Ingredient("Ingredient " + Integer.toString(i), Integer.toString(i) + "oz"));
+                mSeafoodList.add(new Ingredient("Ingredient " + Integer.toString(i), Integer.toString(i) + "oz"));
+                mGroceryList.add(new Ingredient("Ingredient " + Integer.toString(i), Integer.toString(i) + "oz"));
+                mDairyList.add(new Ingredient("Ingredient " + Integer.toString(i), Integer.toString(i) + "oz"));
             }
+
+            adapter = new ShoppingListAdapter(getActivity().getApplicationContext(), mShoppingList); //object to update fragment
+
+            mShoppingList.add("Produce");
+            mShoppingList.addAll(mProduceList);
+            mShoppingList.add("Bakery");
+            mShoppingList.add(mBakeryList);
+            mShoppingList.add("Deli");
+            mShoppingList.add(mDeliList);
+            mShoppingList.add("Meat");
+            mShoppingList.add(mMeatList);
+            mShoppingList.add("Seafood");
+            mShoppingList.add(mSeafoodList);
+            mShoppingList.add("Grocery");
+            mShoppingList.add(mGroceryList);
+            mShoppingList.add("Dairy");
+            mShoppingList.add(mDairyList);
+
+            Log.v("ShoppingListFragment", "onCreate....DONE");
         }
     }
 
+    private View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_shopping_list, container, false);
+        mShoppingListView = (ListView) view.findViewById(R.id.shoppingListView);
+        mShoppingListView.setAdapter(adapter);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping_list, container, false);
+        Log.v("ShoppingListFragment", "onCreateView....DONE");
+        return view;
     }
 
 }
